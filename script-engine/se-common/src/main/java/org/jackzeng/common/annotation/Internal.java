4d4375079e49dafa3b164b5b759a66b75a412bd6
xijin.zeng <xijin.zeng@bkjk.com>
2018-10-18 14:11:50 +0800

    add

---
+++ script-engine/se-common/src/main/java/org/jackzeng/common/annotation/Internal.java
@@ -0,0 +1,14 @@
+package org.jackzeng.common.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * @author xijin.zeng created on 2018/10/18
+ */
+@Documented
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR})
+@Public
+public @interface Internal {
+}
